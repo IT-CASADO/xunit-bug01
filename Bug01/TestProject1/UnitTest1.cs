c1c37bcefa251cd5f807e8e29e0e0239b673653d
Daniel Casado <Daniel.Casado@Evolutionizer.com>
2025-01-08 17:27:49 +0100

    try to reproduce NCrunch issue with Aspose.Slides

--- Bug01/TestProject1/UnitTest1.cs
+++ Bug01/TestProject1/UnitTest1.cs
@@ -1,11 +1,29 @@
+using Aspose.Slides;
+
 namespace TestProject1
 {
-	public class UnitTest1
+	public class UnitTest1 : IDisposable
 	{
+		private readonly IPresentation _presentation;
+
+		public UnitTest1()
+		{
+			_presentation = new Aspose.Slides.Presentation();
+		}
+
 		[Fact]
 		public void Test1()
 		{
 			Assert.Equal(1, 1);
 		}
+
+		[Fact]
+		public void Test_for_Aspose()
+		{
+			Assert.Equal(1, 1);
+		}
+
+		public void Dispose() => _presentation.Dispose();
+
 	}
 }
