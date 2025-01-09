01b2d9ad38a94faefc6355a6f65e116afe1542b3
Daniel Casado <Daniel.Casado@Evolutionizer.com>
2025-01-09 21:31:20 +0100

    implement IAsyncLifetime, IDisposable

--- Bug01/TestProject1/UnitTest1.cs
+++ Bug01/TestProject1/UnitTest1.cs
@@ -1,11 +1,35 @@
 namespace TestProject1
 {
-	public class UnitTest1
+	public class UnitTest1 : IAsyncLifetime, IDisposable
 	{
+		private readonly ITestOutputHelper _testOutputHelper;
+
+		public UnitTest1(ITestOutputHelper testOutputHelper)
+		{
+			_testOutputHelper = testOutputHelper;
+		}
+
 		[Fact]
 		public void Test1()
 		{
 			Assert.Equal(1, 1);
+		}
+
+		public ValueTask InitializeAsync()
+		{
+			_testOutputHelper.WriteLine("InitializeAsync");
+			return ValueTask.CompletedTask;
+		}
+
+		public ValueTask DisposeAsync()
+		{
+			_testOutputHelper.WriteLine("DisposeAsync");
+			return ValueTask.CompletedTask;
+		}
+
+		public void Dispose()
+		{
+			_testOutputHelper.WriteLine("Dispose");
 		}
 	}
 }
